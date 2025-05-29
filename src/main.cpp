29245ccea2fccc290338b282567d191ee2809358
unknown <st182324@stud.uni-stuttgart.com>
2025-05-29 17:09:23 +0200

    Initialer Commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(100);
+  int result = myFunction(2, 3);
+  Serial.println("Result of myFunction(2, 3): ");
+  Serial.println(result);
+  Serial.println("Setup complete.");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
